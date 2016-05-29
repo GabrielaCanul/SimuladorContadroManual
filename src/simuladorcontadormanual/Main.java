9b8557ce1f05996775a1913517f98a8986eb3af4
GABY CANUL <GABY CANUL>
2016-05-28 19:13:41 -0500

    SimuladorContadorManual: Se agregò interfaz de Usuario grafica. No incluye manejo de eventos

--- src/simuladorcontadormanual/Main.java
+++ src/simuladorcontadormanual/Main.java
@@ -5,6 +5,7 @@
 package simuladorcontadormanual;
 
 import contadormanual.ContadorManual;
+import gui.MainForm;
 
 /**
  *
@@ -16,20 +17,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ContadorManual contadorDePasajeros = new ContadorManual();
-        
-        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
-        contadorDePasajeros.contar();
-        System.out.println("Click");
-        contadorDePasajeros.contar();
-        System.out.println("Click");
-        contadorDePasajeros.contar();
-        System.out.println("Click");
-        contadorDePasajeros.contar();
-        System.out.println("Click");
-        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
-        System.out.println("Se reiniciará el conteo de pasajeros...");
-        contadorDePasajeros.reiniciarConteo();
-        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+        MainForm windows = new MainForm();
+        windows.setVisible(true);
+//        ContadorManual contadorDePasajeros = new ContadorManual();
+//        
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+//        System.out.println("Se reiniciará el conteo de pasajeros...");
+//        contadorDePasajeros.reiniciarConteo();
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
     }
 }
